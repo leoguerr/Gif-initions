23832bddfedd671ab2591b01d6b257895ecc7864
eriksvetlik <svetlik.erik@gmail.com>
2022-01-11 13:56:15 -0600

    added comments, removed console logs and added functionality to display content on page

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,137 @@
 // var search = document.getElementById("searchResults").value;
 var GiphyAPIKey = "69YdDv5rJNqo0tpHseZP6XyqSihO6MzA";
 var search = "";
-var words = ["Annihilate", "Complacent", "Conceive", "Confine", "Demonstrate", "Deplete", "Desolate", "Erratic", "Excel", "Exhilarating", "Frankly", "Geriatric", "Hostile", "Lucrative", "Ornate", "Petulant", "Reluctantly", "Scrutinize", "Solidarity", "Unveil", "Contemplate", "Dreary", "Dubious", "Bittersweet", "Diminish" ];
+var words = [
+  "Annihilate",
+  "Complacent",
+  "Conceive",
+  "Confine",
+  "Demonstrate",
+  "Deplete",
+  "Desolate",
+  "Erratic",
+  "Excel",
+  "Exhilarating",
+  "Frankly",
+  "Geriatric",
+  "Hostile",
+  "Lucrative",
+  "Ornate",
+  "Petulant",
+  "Reluctantly",
+  "Scrutinize",
+  "Solidarity",
+  "Unveil",
+  "Contemplate",
+  "Dreary",
+  "Dubious",
+  "Bittersweet",
+  "Diminish",
+];
 
 function randomWord() {
   search = words[Math.floor(Math.random() * words.length)];
   console.log("search " + search);
   return search;
-};
+}
 
 randomWord();
 
-function DictionaryAPI() {
+// function to fetch Free Dictionary API
+function FreeDictionaryAPI() {
   var requestUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
 
   fetch(requestUrl)
     .then(function (response) {
+      // if the the HTTP status code of the response is 404, the user is redirected
+      // to 404.html
       if (response.status === 404) {
-        console.log(response.status);
-        return;
+        window.location.replace("404.html");
       }
       return response.json();
     })
     .then(function (data) {
       console.log(data);
 
+      // returns the searched word
+      var word = document.createElement("p");
+      word.textContent = data[0].word;
+      wordInfo.append(word);
+
+      // returns the pronunciation of the searched word
+      var pronunciation = document.createElement("p");
+      pronunciation.textContent = data[0].phonetics[0].text;
+      wordInfo.append(pronunciation);
+
+      // returns the related pronunciation audio file
+      var audio = document.createElement("audio");
+      var controls = document.createAttribute("controls");
+      audio.setAttributeNode(controls);
+
+      var source = document.createElement("source");
+      source.src = "https:" + data[0].phonetics[0].audio;
+      source.type = "audio/mpeg";
+
+      wordInfo.append(audio);
+      audio.appendChild(source);
+
       for (var i = 0; i < data[0].meanings.length; i++) {
-        var grammerArray = data[0].meanings[i].definitions;
-        console.log("DEFINITION");
-        grammerArray.forEach((grammer) => {
-          console.log(grammer.definition);
+        // returns whether the searched word is a noun, verb, etc.
+        var partOfSpeech = document.createElement("p");
+        partOfSpeech.textContent = data[0].meanings[i].partOfSpeech;
+
+        wordInfo.append(partOfSpeech);
+
+        // returns the definitions of the searched word
+        var definitionsArray = data[0].meanings[i].definitions;
+        definitionsArray.forEach((definition) => {
+          var definitions = document.createElement("p");
+          definitions.textContent = definition.definition;
+
+          wordInfo.append(definitions);
         });
       }
-
-      var phoneticsArray = data[0].phonetics[0];
-      console.log("PHONETICS");
-      console.log(phoneticsArray.text);
-      console.log("PHONETICSAUDIO");
-      console.log(phoneticsArray.audio.slice(2));
-
-      var word = data[0].word;
-      console.log("WORD");
-      console.log(word);
     });
 }
 
+// function to fetch GIPHY API
 function GiphyAPI() {
   var requestUrl =
     "http://api.giphy.com/v1/gifs/search?q=" +
     search +
     "&api_key=" +
     GiphyAPIKey +
-    "&limit=5";
+    "&limit=25";
 
   fetch(requestUrl)
     .then(function (response) {
+      // if the the HTTP status code of the response is 404, the user is redirected
+      // to 404.html
       if (response.status === 404) {
-        console.log(response.status);
-        return;
+        window.location.replace("404.html");
       }
       return response.json();
     })
     .then(function (data) {
       console.log(data.data);
+      // this returns the image url of the first 25 related gifs
       for (var i = 0; i < data.data.length; i++) {
-        console.log(data.data[i].embed_url);
+        var cardDiv = document.createElement("div");
+        cardDiv.setAttribute("class", "card");
+        cardDiv.style.margin = "10px";
+
+        var cardImg = document.createElement("div");
+        cardImg.setAttribute("class", "card-image");
+
+        var img = document.createElement("img");
+        img.src = data.data[i].images.fixed_height.url;
+
+        cardSection.append(cardDiv);
+        cardDiv.append(cardImg);
+        cardImg.append(img);
       }
     });
 }
 
-//   searchForm.addEventListener("submit", DictionaryAPI);
-//   searchForm.addEventListener("submit", GiphyAPI);
-DictionaryAPI();
+FreeDictionaryAPI();
 GiphyAPI();
